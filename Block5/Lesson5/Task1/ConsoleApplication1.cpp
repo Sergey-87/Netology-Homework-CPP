602a8c07091c4bc824116e9e021847c3cbb434da
Sergey Proskurin <sergei_proskurin87@mail.ru>
2024-09-03 00:55:09 +0300

    add Lesson5 homeworks

---
+++ Block5/Lesson5/Task1/ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+template <class V> 
+  void square(std::vector<V>& vec) {
+    for (V& elem : vec) {
+        elem *= elem;
+    }
+}
+template <class V> 
+  void print_vector(std::vector<V>& vec) {
+    bool is_first = true;
+    for (int elem : vec) {
+        if (is_first) {
+            std::cout << elem;
+            is_first = false;
+        }
+        else
+            std::cout << ", " << elem;
+    }
+    std::cout << std::endl;
+}
+template <class O> 
+  int square(O num) { 
+      return num *= num; 
+  }
+
+int main() {
+    int num = 4;
+    std::cout << "[IN]: " << num;
+    std::cout << std::endl;
+    std::cout << "[OUT]: " << square(num) << std::endl;
+    std::vector<int> vec = { -1, 4, 8 };
+    std::cout << "[IN]: ";
+    print_vector(vec);
+    square(vec);
+    std::cout << "[OUT]: ";
+    print_vector(vec);
+    return 0;
+}
