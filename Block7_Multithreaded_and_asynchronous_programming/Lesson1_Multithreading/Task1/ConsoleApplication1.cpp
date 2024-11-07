6b3ee35aaf8bbd72b40807e6026e098878323c7e
Sergey Proskurin <sergei_proskurin87@mail.ru>
2024-11-07 22:31:29 +0300

    add Block7 Lesson1 homeworks

---
+++ Block7_Multithreaded_and_asynchronous_programming/Lesson1_Multithreading/Task1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+int maxClients = 5;
+int clientCounter = 0;
+
+void clientThread() {
+    while (clientCounter < maxClients)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        clientCounter++;
+        std::cout << "Новый клиент, текущее число клиентов: " << clientCounter << std::endl;
+    }
+}
+
+void operatorThread() {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        if (clientCounter > 0)
+        {
+            clientCounter--;
+            std::cout << "Клиент обслужен, осталось клиентов: " << clientCounter << std::endl;
+        }
+        else
+        {
+            std::cout << "Оператор выполнил свою работу, осталось клиентов: " << clientCounter << std::endl;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::thread client(clientThread);
+    std::thread operator_(operatorThread);
+
+    client.join();
+    operator_.join();
+
+    return 0;
+}
---
+++ Block7_Multithreaded_and_asynchronous_programming/Lesson1_Multithreading/Task2/ConsoleApplication2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <chrono>
+#include <iomanip>
+
+static void sumVectors(double sumOfVectors, const std::vector<int> newVector1, const std::vector<int> newVector2)
+{
+    for (int i = 0; i < newVector1.size(); ++i)
+    {
+        sumOfVectors += newVector1.at(i) + newVector2.at(i);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Количество аппаратных ядер: " << std::thread::hardware_concurrency() << "\n\n";
+    std::cout << std::setw(15) << "1000" << "\t\t   10000" << "\t  100000" << "\t 1000000";
+
+    std::vector<int> vector1;
+    std::vector<int> vector2;
+    std::vector<int> numOfThreads{ 1, 2, 4, 8, 16 };
+    std::vector<int> sizeOfVector{ 1000, 10000, 100000, 1000000 };
+
+    for (auto& countThread : numOfThreads)
+    {
+        
+        std::cout << std::endl << countThread << " потоков";
+        
+        for (auto& countVector : sizeOfVector)
+        {
+            vector1.resize(countVector, 1);
+            vector2.resize(countVector, 2);
+            std::vector<std::thread> threads;
+            double sumOfVectors = 0;
+
+            int partOfTheSize = static_cast<int>(countVector / countThread);
+
+            auto start = std::chrono::high_resolution_clock::now();
+            for (int i = 0; i < countThread; i++)
+            {
+                std::vector<int> newVector1;
+                std::vector<int> newVector2;
+                int rightBorder = 0;
+                if (i != countThread - 1)
+                {
+                    rightBorder = partOfTheSize * (i + 1);
+                }
+                else
+                {
+                    rightBorder = countVector;
+                }
+
+                for (int j = partOfTheSize * i; j < rightBorder; ++j)
+                {
+                    newVector1.push_back(vector1.at(j));
+                    newVector2.push_back(vector2.at(j));
+                }
+                threads.push_back(std::thread(sumVectors, std::ref(sumOfVectors), newVector1, newVector2));
+            }
+            for (auto& it : threads)
+            {
+                it.join();
+            }
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> totalTime = end - start;
+            std::cout << " " << totalTime.count() << "s\t";
+        }
+    }
+    std::cout << std::endl;
+    return 0;
+}
