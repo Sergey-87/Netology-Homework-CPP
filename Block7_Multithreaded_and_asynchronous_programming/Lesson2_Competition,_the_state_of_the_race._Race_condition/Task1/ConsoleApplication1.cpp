73a0deb997072a9b6950c7ddd9764288eaa731f9
Sergey Proskurin <sergei_proskurin87@mail.ru>
2024-11-11 21:26:17 +0300

    add Lesson2 Homeworks

---
+++ Block7_Multithreaded_and_asynchronous_programming/Lesson2_Competition,_the_state_of_the_race._Race_condition/Task1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <atomic>
+
+int maxClients = 5;
+std::atomic<int> clientCounter(0);
+
+void clientThread() {
+    while (clientCounter < maxClients)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        clientCounter.fetch_add(1, std::memory_order_seq_cst);
+        std::cout << "Новый клиент, текущее число клиентов: " << clientCounter << std::endl;
+    }
+}
+
+void operatorThread() {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        if (clientCounter > 0)
+        {
+            clientCounter.fetch_sub(1, std::memory_order_seq_cst);
+            std::cout << "Клиент обслужен, осталось клиентов: " << clientCounter << std::endl;
+        }
+        else
+        {
+            std::cout << "Оператор выполнил свою работу, осталось клиентов: " << clientCounter << std::endl;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::thread client(clientThread);
+    std::thread operator_(operatorThread);
+
+    client.join();
+    operator_.join();
+
+    return 0;
+}
