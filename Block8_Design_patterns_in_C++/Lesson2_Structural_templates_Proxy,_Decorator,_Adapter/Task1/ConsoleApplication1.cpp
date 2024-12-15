0ce87936261334420e22e76c8640f6779e4ec49d
Sergey Proskurin <sergei_proskurin87@mail.ru>
2024-12-15 21:10:27 +0300

    add Lesson2 Homeworks

---
+++ Block8_Design_patterns_in_C++/Lesson2_Structural_templates_Proxy,_Decorator,_Adapter/Task1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "decorator.h"
+
+int main()
+{
+    auto paragraph_block = new Paragraph(new Text());
+    paragraph_block->render("Hello world");
+    std::cout << std::endl;
+
+    auto reverse_text_block = new Reversed(new Text());
+    reverse_text_block->render("Hello world");
+    std::cout << std::endl;
+
+    auto link_block = new Link(new Text());
+    link_block->render("netology.ru", "Hello world");
+    std::cout << std::endl;
+
+    return 0;
+}
