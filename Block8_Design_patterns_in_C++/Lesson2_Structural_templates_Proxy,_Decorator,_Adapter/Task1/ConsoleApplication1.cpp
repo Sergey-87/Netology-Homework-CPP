c3eaa1e644391cc44ec373c3412e1aa4bd897e6c
Sergey Proskurin <sergei_proskurin87@mail.ru>
2024-12-17 21:56:18 +0300

    Task1 fixed

--- Block8_Design_patterns_in_C++/Lesson2_Structural_templates_Proxy,_Decorator,_Adapter/Task1/ConsoleApplication1.cpp
+++ Block8_Design_patterns_in_C++/Lesson2_Structural_templates_Proxy,_Decorator,_Adapter/Task1/ConsoleApplication1.cpp
@@ -1,17 +1,21 @@
 ﻿#include <iostream>
 #include "decorator.h"
+#include <memory>
 
 int main()
 {
-    auto paragraph_block = new Paragraph(new Text());
+    std::shared_ptr<Paragraph> paragraph_block = std::make_shared<Paragraph>(new Text);
+    /*auto paragraph_block = new Paragraph(new Text());*/
     paragraph_block->render("Hello world");
     std::cout << std::endl;
 
-    auto reverse_text_block = new Reversed(new Text());
+    std::shared_ptr<Reversed> reverse_text_block = std::make_shared<Reversed>(new Text);
+    /*auto reverse_text_block = new Reversed(new Text());*/
     reverse_text_block->render("Hello world");
     std::cout << std::endl;
 
-    auto link_block = new Link(new Text());
+    std::shared_ptr<Link> link_block = std::make_shared<Link>(new Text);
+    /*auto link_block = new Link(new Text());*/
     link_block->render("netology.ru", "Hello world");
     std::cout << std::endl;
 
